9da7ad27c529d4cea2aa60f3c924c4125fa6d22c
João Pedro Zanqueta <joaopzta@gmail.com>
2021-01-06 13:38:24 -0300

    Add Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Home from "./Home";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <>
+        <Navbar />
+        <Home />
+      </>
     </div>
   );
 }
